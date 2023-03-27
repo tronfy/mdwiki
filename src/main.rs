1c82b26fe2383c27cb7d24e50fe05610507aa894
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2023-03-27 19:23:13 -0300

    build indexes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::fs::{self, File};
+use std::collections::HashMap;
+use std::fs::{self, File, OpenOptions};
+use std::io::Write;
 use std::io::{BufRead, BufReader};
 use std::os::unix::prelude::FileExt;
 use std::path::Path;
@@ -9,44 +11,69 @@ use sanitize::to_md;
 use crate::sanitize::text_from;
 
 fn main() -> std::io::Result<()> {
-    let base_path = "/home/tronfy/source/wikirun/data/";
-    let dump = "ptwiki-articles.xml";
+    let base_path = "../data/out/";
+    let dump = "../raw/ptwiki-20230320-pages-articles.xml";
     let file = File::open(base_path.to_owned() + dump).unwrap();
-    let reader = BufReader::new(file);
 
     fs::create_dir_all(base_path.to_owned() + "wiki")?;
 
     let mut page = String::new();
     let mut title = String::new();
+    let mut id: u32 = 0;
 
+    let mut indexes: HashMap<u32, String> = HashMap::new();
+
+    let reader = BufReader::new(file);
     for line in reader.lines() {
         let line = line? + "\n";
 
         if line.trim().starts_with("<page>") {
             page = String::new();
             title = String::new();
+            id = 0;
         } else if line.trim().starts_with("<title>") {
             title = line
                 .replace("<title>", "")
                 .replace("</title>", "")
-                .replace("/", "-")
                 .trim()
+                .replace("/", "\\")
+                .replace(" ", "_")
                 .to_string();
-
-            let v = title.chars().collect::<Vec<_>>(); // I □ unicode
-            if v.len() > 100 {
-                title = v[..100].iter().cloned().collect::<String>();
-            }
+        } else if line.trim().starts_with("<id>") && id == 0 {
+            id = line
+                .replace("<id>", "")
+                .replace("</id>", "")
+                .trim()
+                .parse::<u32>()
+                .unwrap();
         } else if line.trim().starts_with("</page>") {
-            let f = format!("{}wiki/{}.md", base_path.to_owned(), title);
+            if !title.contains(":") {
+                let f = format!("{}wiki/{}.md", base_path.to_owned(), id);
 
-            if !Path::new(&f).exists() && !title.contains(":") {
-                let file = File::create(f)?;
-                file.write_all_at(to_md(text_from(&page), &title).as_bytes(), 0)?;
+                if !Path::new(&f).exists() {
+                    indexes.insert(id, title.clone());
+                    let file = File::create(f)?;
+                    file.write_all_at(
+                        to_md(text_from(&page), &title.replace("_", " ")).as_bytes(),
+                        0,
+                    )?;
+                }
             }
         }
 
         page += &line;
+    }
+
+    let mut index_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(base_path.to_owned() + "indexes.csv")
+        .unwrap();
+
+    writeln!(index_file, "id,title").unwrap();
+    for (id, title) in indexes {
+        writeln!(index_file, "{},{}", id, title).unwrap();
     }
 
     Ok(())
